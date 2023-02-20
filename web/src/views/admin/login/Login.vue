8faf712962320dc1cf5cb38b968ccb2a48c8c3ce
YuHua <316910279@qq.com>
2023-02-20 13:19:38 +0800

    登录

--- web/src/views/admin/login/Login.vue
+++ web/src/views/admin/login/Login.vue
@@ -1,13 +1,99 @@
 <template>
-    <div class="l-back"></div>
+  <div class="l-back">
+    <div class="box">
+      <h1>登录</h1>
+      <el-form :model="model" status-icon :rules="rules" ref="form">
+        <el-form-item label="用户名" prop="username">
+          <el-input
+            v-model="model.username"
+            clearable
+            placeholder="请输入用户名"
+            onkeyup="value=value.replace(/[^\w_]/g,'')"
+          ></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input
+            type="password"
+            v-model="model.password"
+            clearable
+            show-password
+            placeholder="请输入密码"
+          ></el-input>
+        </el-form-item>
+        <el-form-item>
+          <div class="button">
+            <el-button type="primary" @click="login">登录</el-button>
+          </div>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "AdminLogin"
-}
+  name: "AdminLogin",
+  data() {
+    return {
+      model: {
+        username: null,
+        password: null,
+      },
+      rules: {
+        username: [
+          {
+            required: true,
+            message: "请输入用户名！",
+            trigger: "blur",
+          },
+          { min: 5, max: 10, message: "长度在5到10个字符！", trigger: "blur" },
+        ],
+        password: [
+          {
+            required: true,
+            message: "请输入密码！",
+            trigger: "blur",
+          },
+          { min: 5, max: 10, message: "密码长度不正确！", trigger: "blur" },
+        ],
+      },
+    };
+  },
+  methods: {
+    login() {
+      this.$refs.form.validate((valid) => {
+        if (valid) {
+            this.$router.push("/admin-list");
+        } else {
+          return false;
+        }
+      });
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-
+.l-back {
+  display: flex;
+  height: 100%;
+  width: 100%;
+  .box {
+    margin: auto;
+    width: 300px;
+    background-color: white;
+    box-shadow: 0 16px 32px 0 rgba(0, 0, 0, 0.08);
+    border-radius: 32px;
+    padding: 20px;
+    h1 {
+      font-size: 40px;
+    }
+    .button {
+      width: 100%;
+      display: flex;
+      justify-content: center;
+      margin-top: 20px;
+    }
+  }
+}
 </style>
