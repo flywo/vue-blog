2e77ad63850248894eb9fa246b60f97565b3bc70
YuHua <316910279@qq.com>
2023-02-21 18:27:53 +0800

    博客列表

--- web/src/views/admin/list/List.vue
+++ web/src/views/admin/list/List.vue
@@ -18,7 +18,20 @@
         </li>
       </ul>
     </div>
-    <div class="blog-list"></div>
+    <div class="blog-list">
+      <div class="add">新建文章</div>
+      <ul>
+        <li
+          v-for="(item, index) in blogs"
+          :key="'blog-' + index"
+          :class="index === currentBlog ? 'blog-current' : ''"
+        >
+          <i class="el-icon-delete-solid more"></i>
+          <span>{{ item.title }}</span>
+          <span>{{ item.preview }}</span>
+        </li>
+      </ul>
+    </div>
     <div class="blog-content"></div>
   </div>
 </template>
@@ -30,6 +43,21 @@ export default {
     return {
       types: ["1", "2"],
       currentType: 0,
+      blogs: [
+        {
+          title: "标题",
+          preview: "预览",
+        },
+        {
+          title: "标题",
+          preview: "预览",
+        },
+        {
+          title: "标题",
+          preview: "预览",
+        },
+      ],
+      currentBlog: 0,
     };
   },
   methods: {
@@ -89,6 +117,59 @@ export default {
     width: 20%;
     height: 100%;
     overflow-y: auto;
+    border-right: 1px solid #d9d9d9;
+    .add {
+      line-height: 20px;
+      font-size: 15px;
+      font-weight: 400;
+      padding: 20px 0 20px 25px;
+      cursor: pointer;
+      color: #595959;
+      text-align: center;
+    }
+    ul {
+      list-style: none;
+      padding-left: 0;
+      margin-top: 0;
+      margin-bottom: 0;
+      li {
+        cursor: pointer;
+        height: 90px;
+        border-left: 5px solid transparent;
+        box-shadow: 0 0 0 1px #d9d9d9;
+        padding-left: 20px;
+        padding-block: 15px;
+        box-sizing: border-box;
+        span {
+          display: block;
+          height: 30px;
+          line-height: 30px;
+          overflow: hidden;
+          -o-text-overflow: ellipsis;
+          text-overflow: ellipsis;
+          white-space: nowrap;
+          font-size: 18px;
+          color: #333;
+        }
+        span:nth-child(3) {
+          color: #595959;
+          font-size: 16px;
+        }
+        .more {
+          float: right;
+          color: #333;
+          margin-inline: 10px;
+          margin-top: 22.5px;
+        }
+      }
+      li:hover {
+        background-color: #e6e6e6;
+      }
+      .blog-current {
+        border-left-color: #ec7259;
+        background-color: #e6e6e6;
+      }
+    }
   }
   .blog-content {
     width: 70%;
