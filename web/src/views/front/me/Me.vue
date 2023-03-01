cc703d3d23c0499ace53a889dab36ae7b2c30b4b
YuHua <316910279@qq.com>
2023-03-01 17:29:42 +0800

    完善关于我

--- web/src/views/front/me/Me.vue
+++ web/src/views/front/me/Me.vue
@@ -2,11 +2,34 @@
   <div class="me-back">
     <div class="content">
       <div class="title">
-        <p>你好，欢迎来到我的博客。我叫<strong>余华</strong>。</p>
+        <strong>余华</strong>
         <p>前端开发 | iOS开发 | Flutter开发 | Node.js开发</p>
       </div>
+      <div class="detail">
+        <strong class="d-title">个人简介</strong>
+        <p>
+          从13年大学毕业开始，一直从事编码工作。一开始搞C语言开发，后来搞iOS开发，搞iOS开发的时间最长，再后来看到了Flutter，于是开始搞Flutter，到现在搞前端，搞了前端过后，发现有Node.js这么个好东西，于是顺带着也会搞后端了。如今，我可以厚着脸皮说一声：<strong
+            >我是全栈！</strong
+          >
+        </p>
+        <el-image
+          style="height: 300px; width: 100%"
+          :src="require('@/assets/me0.png')"
+          fit="contain"
+        ></el-image>
+        <p>
+          有人问我，学这么多东西，不累吗？我回答是：<strong
+            >累？糊口啊，大哥！</strong
+          >
+        </p>
+        <el-image
+          style="height: 300px; width: 100%"
+          :src="require('@/assets/me1.png')"
+          fit="contain"
+        ></el-image>
+      </div>
       <div class="stack">
-        <strong>技术栈</strong>
+        <strong>我的技术栈</strong>
         <div
           class="item"
           v-for="(item, index) in stack"
@@ -31,7 +54,7 @@
         </div>
       </div>
       <div class="experience">
-        <strong>技能简介</strong>
+        <strong>我的技能简介</strong>
         <el-timeline class="time-line">
           <el-timeline-item timestamp="2013" placement="top">
             <el-card>
@@ -59,6 +82,24 @@
             </el-card>
           </el-timeline-item>
         </el-timeline>
+      </div>
+      <div class="contact">
+        <el-image
+          class="icon"
+          fit="contain"
+          :src="require('@/assets/github.png')"
+          @click="jumpUrl({ url: 'https://github.com/flywo' })"
+        ></el-image>
+        <el-image
+          class="icon"
+          fit="contain"
+          :src="require('@/assets/js.png')"
+          @click="jumpUrl({ url: 'https://www.jianshu.com/u/dacb5494231b' })"
+        ></el-image>
+        <el-popover placement="top" trigger="hover">
+          <div style="text-align: center; margin-block: 5px">316910279</div>
+          <div class="qq" slot="reference">QQ</div>
+        </el-popover>
       </div>
     </div>
   </div>
@@ -158,19 +199,31 @@ export default {
     border-radius: 15px;
     box-sizing: border-box;
     padding: 20px;
+    position: relative;
     .title {
-      p:first-child {
+      strong {
+        font-size: 50px;
+        font-weight: bold;
+      }
+      p:last-child {
+        color: #999;
+      }
+    }
+    .detail {
+      margin-top: 40px;
+      .d-title {
+        font-weight: bold;
         font-size: 30px;
-        strong {
-          font-weight: bold;
-        }
+      }
+      p {
+        font-size: 18px;
       }
     }
     .stack {
       margin-top: 40px;
       strong {
         font-weight: bold;
-        font-size: 20px;
+        font-size: 30px;
       }
       .item {
         -webkit-box-shadow: 0px 0px 6px 0px rgba(0, 0, 0, 0.15);
@@ -205,10 +258,35 @@ export default {
       margin-top: 40px;
       strong {
         font-weight: bold;
-        font-size: 20px;
+        font-size: 30px;
       }
       .time-line {
         padding: 20px;
+      }
+    }
+    .contact {
+      position: absolute;
+      top: 20px;
+      right: 20px;
+      display: flex;
+      .icon,
+      .qq {
+        margin: 10px;
+        -webkit-box-shadow: 0px 0px 6px 0px rgba(0, 0, 0, 0.15);
+        box-shadow: 0px 0px 6px 0px rgba(0, 0, 0, 0.15);
+        border-radius: 50%;
+        height: 40px;
+        width: 40px;
+        cursor: pointer;
+        transition: all 0.3s ease-out;
+      }
+      .icon:hover,
+      .qq:hover {
+        transform: scale(1.1, 1.1);
+      }
+      .qq {
+        line-height: 40px;
+        text-align: center;
       }
     }
   }
