29c36846653b0501af0d964c7ccb01d419ceaad0
YuHua <316910279@qq.com>
2023-07-12 21:33:35 +0800

    fix

--- web/src/views/front/more/king/king.vue
+++ web/src/views/front/more/king/king.vue
@@ -26,6 +26,7 @@
 import * as THREE from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import gsap from "gsap";
+import axios from "axios";
 export default {
   name: "KingView",
   data() {
@@ -53,9 +54,6 @@ export default {
     camera.lookAt(1.7, 0, 8.7);
 
     const loadingManager = new THREE.LoadingManager();
-    loadingManager.onProgress = (url, loaded, total) => {
-      this.progress = parseInt((loaded / total) * 100);
-    };
     loadingManager.onLoad = () => {
       this.loading = false;
     };
@@ -65,59 +63,67 @@ export default {
     renderer.toneMapping = THREE.ACESFilmicToneMapping;
 
     let position = 0;
-    gltfLoader.load("/file/glb/king_room.glb", function (gltf) {
-      const model = gltf.scene;
-      scene.add(model);
-
-      window.addEventListener("mouseup", function () {
-        switch (position) {
-          case 0:
-            moveCamera(-1.8, 1.6, 5);
-            rotateCamera(0, 0.1, 0);
-            position = 1;
-            break;
-          case 1:
-            moveCamera(2.8, 0, 3.6);
-            rotateCamera(0, -2, 0);
-            position = 2;
-            break;
-          case 2:
-            moveCamera(2.5, -0.9, 12.2);
-            rotateCamera(0.9, 0.6, -0.6);
-            position = 3;
-            break;
-          case 3:
-            moveCamera(-2.7, 0.6, 3.7);
-            rotateCamera(0.6, 1.9, -0.6);
-            position = 4;
-            break;
-          case 4:
-            moveCamera(-1.7, 0, 8.7);
-            rotateCamera(0, 4.7, 0);
-            position = 5;
-            break;
-          case 5:
-            moveCamera(0.5, 0.8, 10);
-            rotateCamera(0.3, 1.65, -0.3);
-            position = 0;
+    axios({
+      method: "GET",
+      url: "static/glb/king_room.glb",
+      responseType: "blob",
+      onDownloadProgress: (event) => {
+        this.progress = (event.progress * 100).toFixed(2);
+      },
+    }).then((response) => {
+      gltfLoader.load(URL.createObjectURL(response.data), function (gltf) {
+        const model = gltf.scene;
+        scene.add(model);
+        window.addEventListener("mouseup", function () {
+          switch (position) {
+            case 0:
+              moveCamera(-1.8, 1.6, 5);
+              rotateCamera(0, 0.1, 0);
+              position = 1;
+              break;
+            case 1:
+              moveCamera(2.8, 0, 3.6);
+              rotateCamera(0, -2, 0);
+              position = 2;
+              break;
+            case 2:
+              moveCamera(2.5, -0.9, 12.2);
+              rotateCamera(0.9, 0.6, -0.6);
+              position = 3;
+              break;
+            case 3:
+              moveCamera(-2.7, 0.6, 3.7);
+              rotateCamera(0.6, 1.9, -0.6);
+              position = 4;
+              break;
+            case 4:
+              moveCamera(-1.7, 0, 8.7);
+              rotateCamera(0, 4.7, 0);
+              position = 5;
+              break;
+            case 5:
+              moveCamera(0.5, 0.8, 10);
+              rotateCamera(0.3, 1.65, -0.3);
+              position = 0;
+          }
+        });
+        function moveCamera(x, y, z) {
+          gsap.to(camera.position, {
+            x,
+            y,
+            z,
+            duration: 3,
+          });
+        }
+        function rotateCamera(x, y, z) {
+          gsap.to(camera.rotation, {
+            x,
+            y,
+            z,
+            duration: 3.2,
+          });
         }
       });
-      function moveCamera(x, y, z) {
-        gsap.to(camera.position, {
-          x,
-          y,
-          z,
-          duration: 3,
-        });
-      }
-      function rotateCamera(x, y, z) {
-        gsap.to(camera.rotation, {
-          x,
-          y,
-          z,
-          duration: 3.2,
-        });
-      }
     });
 
     function animate() {
